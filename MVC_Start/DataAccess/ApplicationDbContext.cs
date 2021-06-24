85be40512f44cd380a789720d44f893634934772
Jenniferodd <jenniferodd@gmail.com>
2021-06-24 19:17:33 -0400

    create many-many relationship

---
+++ MVC_Start/DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_Start.Models;
+
+namespace MVC_Start.DataAccess
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Station> Stations { get; set; }
+        public DbSet<Park> Parks { get; set; }
+        public DbSet<StationParkDistance> StationParkDistances { get; set; }
+    }
+}
--- MVC_Start/Startup.cs
+++ MVC_Start/Startup.cs
@@ -6,48 +6,53 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using MVC_Start.DataAccess;
+using Microsoft.Extensions.Hosting;
+
 
 namespace MVC_Start
 {
-  public class Startup
-  {
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
     // This method gets called by the runtime. Use this method to add services to the container.
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
     {
-      // Added from MVC template
-      services.AddMvc();
+        services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration["Data:IEXTrading:ConnectionString"]));
+        // Added from MVC template
+        services.AddMvc();
     }
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    // the version below came with the empty template
-    //public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-    //{
-    //  if (env.IsDevelopment())
-    //  {
-    //    app.UseDeveloperExceptionPage();
-    //  }
-
-    //  app.Run(async (context) =>
-    //  {
-    //    await context.Response.WriteAsync("Hello World!");
-    //  });
-    //}
 
     // this is the version from the MVC template. Compare to above for changes
     public void Configure(IApplicationBuilder app, IHostingEnvironment env)
     {
-      if (env.IsDevelopment())
-      {
-        app.UseBrowserLink();
-        app.UseDeveloperExceptionPage();
-      }
-      else
-      {
-        app.UseExceptionHandler("/Home/Error");
-      }
+         if (env.IsDevelopment())
+         {
+           app.UseBrowserLink();
+           app.UseDeveloperExceptionPage();
+         }
+         else
+         {
+           app.UseExceptionHandler("/Home/Error");
+         }
 
-      app.UseStaticFiles();
+    //This ensures that the database and tables are created as per the Models.
+    using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+    {
+        var context = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        context.Database.EnsureCreated();
+    }
+
+    app.UseStaticFiles();
 
       app.UseMvc(routes =>
       {
